3c61cef77fee0dd3e51f461eb3b5329a7ae13afd
Aya Elsayed <45416886+ayaelsayed25@users.noreply.github.com>
2021-12-16 23:03:30 +0200

    Merge

--- Front-end/app/Screens/SignIn.js
+++ Front-end/app/Screens/SignIn.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ThemeContext } from '../Context/ThemeContext';
+import {normalize} from '../util/FontNormalization';
 function SignIn() {
     const {isLightTheme, light, dark} = useContext(ThemeContext);
   return (
         <View style={styles.container}>
-            <Text style={{color: isLightTheme? light.Text : dark.Text}}>Hello</Text>
+            <Text style={{color: isLightTheme? light.Text : dark.Text, fontSize: normalize(30)}}>Hello</Text>
         </View>
   );
 }
@@ -13,7 +14,7 @@ function SignIn() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
