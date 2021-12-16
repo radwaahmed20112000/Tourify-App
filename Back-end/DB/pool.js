0308f337a17377a37ba1f5bc87f58fa79d083e7f
Salaheldinyoussry <salaheldeenyousryabdallah@gmail.com>
2021-12-16 16:48:44 +0200

    feed posts

---
+++ Back-end/DB/pool.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+
+connectionLimit: process.env.DB_CONNECTION_LIMIT,
+user: process.env.DB_USER,
+password: process.env.DB_PASS,
+database: process.env.DB_DATABASE,
+host: process.env.DB_HOST,
+port: process.env.DB_PORT,
+
+})
+
+module.exports = (query) =>{
+    return new Promise((resolve , reject)=>{
+        pool.query(query , (error , result)=>{
+            if(error)
+               return reject(error)
+
+            return resolve(result)
+        })
+    })
+
+}
--- Back-end/Routes/PostRoutes.js
+++ Back-end/Routes/PostRoutes.js
@@ -1,18 +1,28 @@
-import { Dimensions, Platform, PixelRatio } from 'react-native';
+// import { Dimensions, Platform, PixelRatio } from 'react-native';
 
-const {
-  width: SCREEN_WIDTH,
-  height: SCREEN_HEIGHT,
-} = Dimensions.get('window');
+// const {
+//   width: SCREEN_WIDTH,
+//   height: SCREEN_HEIGHT,
+// } = Dimensions.get('window');
 
-// based on iphone 5s's scale
-const scale = SCREEN_WIDTH / 320;
+// // based on iphone 5s's scale
+// const scale = SCREEN_WIDTH / 320;
 
-export function normalize(size) {
-  const newSize = size * scale 
-  if (Platform.OS === 'ios') {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize))
-  } else {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-  }
-}
+// export function normalize(size) {
+//   const newSize = size * scale 
+//   if (Platform.OS === 'ios') {
+//     return Math.round(PixelRatio.roundToNearestPixel(newSize))
+//   } else {
+//     return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+//   }
+// }
+
+
+const express = require('express');
+const router = express.Router();
+let PostController = require('../Controllers/PostController');
+
+
+router.get('/' , PostController.getFeedPosts);
+
+module.exports = router ;
