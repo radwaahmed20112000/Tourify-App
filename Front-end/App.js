2f2edd5d1331b0e23a716c0476ebdbae25d6147e
sarahelshabrawy <hylrcs@gmail.com>
2021-12-23 22:43:30 +0200

    App.js modification

--- Front-end/App.js
+++ Front-end/App.js
@@ -1,58 +1,50 @@
-import React,{ useEffect, useState } from 'react';
-import { StyleSheet, ActivityIndicator, View, Text} from 'react-native';
-import { ThemeContext, Theme} from './app/Context/ThemeContext';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ActivityIndicator, View, Text } from 'react-native';
+import { ThemeContext, Theme } from './app/Context/ThemeContext';
 import Registeration from './app/Screens/Registeration';
 import Feed from './app/Screens/Feed';
-import { NavigationContainer } from '@react-navigation/native';
-import Tabs from './app/Navigation/NavigationTabs';
-
-
 export default function App() {
   const [lightMode, setLightMode] = useState(true)
   const [signUp, setIsSignUp] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
   const [theme, setTheme] = useState(Theme.light);
-
-  function changeSigning()
-  {
+  function changeSigning() {
     setIsSignUp(!signUp);
   }
-  function changeTheme()
-  {
-      setLightMode(!lightMode);
-      if(lightMode) setTheme(Theme.light);
-      else setTheme(Theme.dark);
+  function changeTheme() {
+    setLightMode(!lightMode);
+    if (lightMode) setTheme(Theme.light);
+    else setTheme(Theme.dark);
   }
-  useEffect(()=>{
+  useEffect(() => {
     //check if user logged in????????
   }, [])
 
 
-  return (
-          <ThemeContext.Provider value={{theme, changeTheme}}>
-           <NavigationContainer>
-             <Tabs/>
-             </NavigationContainer>
+  // return (
+  //   <ThemeContext.Provider value={{theme, changeTheme}}>
+  //    <NavigationContainer>
+  //      <Tabs/>
+  //      </NavigationContainer>
 
-          </ThemeContext.Provider>
-        );
+  //   </ThemeContext.Provider>
+  // );
 
-//   if(isLoading)
-//   {
-//     return(
-//       <View style={styles.container}>
-//         <ActivityIndicator size="large" animating={true} color={Theme.light.SecondaryPurple}/>
-//       </View>
-//     )
-//   }
-//   else{
-//     return (
-//       <ThemeContext.Provider value={{theme, changeTheme}}>
-//         <Registeration isSignUp={signUp} changeSigning= {changeSigning}/>
-//         {/* <Feed/> */}
-//       </ThemeContext.Provider>
-//     );
-//   }
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" animating={true} color={Theme.light.SecondaryPurple} />
+      </View>
+    )
+  }
+  else {
+    return (
+      <ThemeContext.Provider value={{ theme, changeTheme }}>
+        <Registeration isSignUp={signUp} changeSigning={changeSigning} />
+        <Feed />
+      </ThemeContext.Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
