a94bba3a195be442c394d9e1208ba901c0ead91c
bothaina hassan <58982280+bothaina1@users.noreply.github.com>
2021-12-24 06:38:43 +0200

    sign up

---
+++ Back-end/Models/Account.js
@@ -0,0 +1,63 @@
+const DB = require("../DB/pool");
+const tableName = 'user';
+module.exports = {
+
+    tableName: tableName,
+
+
+
+    findEmail : async ( email, cb )=>{
+    
+        email =email || '';
+      
+         let selectQuery = `SELECT * FROM ${tableName} WHERE  email = ${email} ;`;
+    
+         try {
+             let user = await DB(selectQuery)
+             console.log("user : ", user)
+             return cb(null, user);
+    
+         } catch (e) {
+             console.log(e)
+             return cb(e, null);
+    
+         }
+    
+     
+     },
+
+
+
+    create: async (account, cb) => {
+
+
+        let insertQuery = `INSERT INTO ${tableName}  VALUES  (${account.email},${account.name},${account.password},${account.photo},${account.googleBool} ) ;`;
+
+        try {
+            let user = await DB(insertQuery)
+            console.log("inserted account : ", user)
+            return cb(null, user);
+
+        } catch (e) {
+            console.log(e)
+            return cb(e, null);
+
+        }
+
+
+    },
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
