f65536665a79c14d13a99396fd7a6cc12082fc67
sarayasser-coder <sara_yasserii@yahoo.com>
2022-01-03 15:52:21 +0200

    editPost

--- Back-end/test/index.test.js
+++ Back-end/test/index.test.js
@@ -1,7 +1,39 @@
-before(function (done) {
+require('dotenv').config();
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+chai.use(chaiHttp);
+const jwt = require("jsonwebtoken");
+const DB = require("../DB/pool");
+
+before( function (done) {
+    this.timeout(200000);
+    let query = `insert  ignore into  user(email, name, password) values('testuser@gmail.com', 'testuser', '$2b$12$DeUOYwsPrHEikbInuJ8LheMIUkj6Wz.qosH8Y9W7w1HeHy0Byfp8W');`
+    DB(query).then(()=>{
+    let body = {
+        email: 'testuser@gmail.com',
+        password: 'salah',
+    }
+
+    chai.request(server)
+        .post(`/account/login`)
+        .send(body)
+        .end((err, res) => {
+            if(err)
+                done(err)
+          console.log("token: ",res.body.token)
+          process.env.TEST_TOKEN = res.body.token
+          done();
+        });
+
+    }).catch(e => {
+        console.log(e)
+    })
+
 
     // Increase the Mocha timeout 
-    this.timeout(20000);
+
 
 })
 
