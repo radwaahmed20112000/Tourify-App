4cac8e93e91a19b64134b81f8060b53cff1667fc
radwaahmed20112000 <radwaahmed20112000@gmail.com>
2022-02-08 06:22:44 +0200

    Get All Notification tests

--- Front-end/app/Screens/Notifications.js
+++ Front-end/app/Screens/Notifications.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState }  from 'react';
 import { SafeAreaView } from 'react-native';
-
 import { getAllNotifications } from '../API/NotificatonAPI';
 import Spinner from 'react-native-loading-spinner-overlay';
 import { TokenContext } from '../Context/TokenContext';
