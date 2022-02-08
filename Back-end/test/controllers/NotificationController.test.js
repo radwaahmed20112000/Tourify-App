7df6e933fdd22ef8f7a290e5f07289637298e239
SaraYasserMohamed <es-sara.yasser2018@alexu.edu.eg>
2022-02-08 23:42:15 +0200

    ..

--- Back-end/test/controllers/NotificationController.test.js
+++ Back-end/test/controllers/NotificationController.test.js
@@ -1,36 +1,36 @@
-// let chai = require('chai');
-// let chaiHttp = require('chai-http');
-// let server = require('../../server');
-// let should = chai.should();
-// chai.use(chaiHttp);
-// const jwt = require("jsonwebtoken");
-// const DB = require("../../DB/pool");
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../../server');
+let should = chai.should();
+chai.use(chaiHttp);
+const jwt = require("jsonwebtoken");
+const DB = require("../../DB/pool");
 
-// describe('Notification controller', function () {
-//     describe('GET /notifications/', function () {
+describe('Notification controller', function () {
+    describe('GET /notifications/', function () {
 
-//         it('it should retrun an array of notifications', (done) => {
-//             chai.request(server)
-//                 .get(`/notifications/`)
-//                 .set('authorization', process.env.TEST_TOKEN)
-//                 .end((err, res) => {
-//                     res.should.have.status(200);
-//                     res.body.should.be.a('array');
+        it('it should retrun an array of notifications', (done) => {
+            chai.request(server)
+                .get(`/notifications/`)
+                .set('authorization', process.env.TEST_TOKEN)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
 
-//                     let notifications = res.body ; 
-//                     notifications.forEach (n => {
-//                         n.should.have.property('id');
-//                         n.should.have.property('photo');
-//                         n.should.have.property('name');
-//                         n.should.have.property('post_id');
-//                         n.should.have.property('comment_id');
-//                         n.should.have.property('sender_id');
-//                         n.should.have.property('created_at');
-//                         n.should.have.property('viewed');
-//                     })
-//                     done();
-//                 });
-//         });
+                    let notifications = res.body ; 
+                    notifications.forEach (n => {
+                        n.should.have.property('id');
+                        n.should.have.property('photo');
+                        n.should.have.property('name');
+                        n.should.have.property('post_id');
+                        n.should.have.property('comment_id');
+                        n.should.have.property('sender_id');
+                        n.should.have.property('created_at');
+                        n.should.have.property('viewed');
+                    })
+                    done();
+                });
+        });
 
-//     });        
-// });   
+    });        
+});   
